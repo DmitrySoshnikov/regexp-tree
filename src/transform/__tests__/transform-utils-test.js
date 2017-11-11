adf103a9efff91eea8242ecb935f6c0406d02d52
Dmitry Soshnikov <dmitrys@fb.com>
2017-11-11 11:25:26 -0800

    Transform: add transform utils

---
+++ src/transform/__tests__/transform-utils-test.js
@@ -0,0 +1,39 @@
+/**
+ * The MIT License (MIT)
+ * Copyright (c) 2017-present Dmitry Soshnikov <dmitry.soshnikov@gmail.com>
+ */
+
+const parser = require('../../parser');
+const transformUtils = require('../utils');
+
+const {TransformResult, transform} = require('..');
+
+describe('transform-utils', () => {
+
+  it('disjunctionToList', () => {
+    const disjunction = parser.parse('/a|b|c|d/').body;
+    const list = transformUtils.disjunctionToList(disjunction);
+
+    expect(list).toEqual([
+      disjunction.left.left.left,  // a
+      disjunction.left.left.right, // b
+      disjunction.left.right,      // c
+      disjunction.right,           // d
+    ]);
+  });
+
+  it('disjunctionToList', () => {
+    const list = [
+      {type: 'Char', value: 'a', kind: 'simple', codePoint: 97, symbol: 'a'},
+      {type: 'Char', value: 'b', kind: 'simple', codePoint: 98, symbol: 'b'},
+      {type: 'Char', value: 'c', kind: 'simple', codePoint: 99, symbol: 'c'},
+      {type: 'Char', value: 'd', kind: 'simple', codePoint: 100, symbol: 'd'},
+    ];
+
+    const disjunction = transformUtils.listToDisjunction(list);
+    const expected = parser.parse('/a|b|c|d/').body;
+
+    expect(disjunction).toEqual(expected);
+  });
+
+});
---
+++ src/transform/utils.js
@@ -0,0 +1,51 @@
+/**
+ * The MIT License (MIT)
+ * Copyright (c) 2017-present Dmitry Soshnikov <dmitry.soshnikov@gmail.com>
+ */
+
+'use strict';
+
+/**
+ * Flattens a nested disjunction node to a list.
+ *
+ * /a|b|c|d/
+ *
+ * {{{a, b}, c}, d} -> [a, b, c, d]
+ */
+function disjunctionToList(node) {
+  if (node.type !== 'Disjunction') {
+    throw new TypeError(`Expected "Disjunction" node, got "${node.type}"`);
+  }
+
+  const list = [];
+
+  if (node.left.type === 'Disjunction') {
+    list.push(...disjunctionToList(node.left), node.right);
+  } else {
+    list.push(node.left, node.right);
+  }
+
+  return list;
+}
+
+/**
+ * Builds a nested disjunction node from a list.
+ *
+ * /a|b|c|d/
+ *
+ * [a, b, c, d] -> {{{a, b}, c}, d}
+ */
+function listToDisjunction(list) {
+  return list.reduce((left, right) => {
+    return {
+      type: 'Disjunction',
+      left,
+      right,
+    }
+  });
+}
+
+module.exports = {
+  disjunctionToList,
+  listToDisjunction,
+};
