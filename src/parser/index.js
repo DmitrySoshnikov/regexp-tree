2af8f847c9e58088daa502f77cbebae37c0b26a4
Dmitry Soshnikov <dmitrys@fb.com>
2018-01-01 12:18:11 -0800

    Parser: accept strings and RegExp instances in parser.parse

--- src/parser/index.js
+++ src/parser/index.js
@@ -7,6 +7,21 @@
 
 const regexpTreeParser = require('./generated/regexp-tree');
 
+/**
+ * Original parse function.
+ */
+const generatedParseFn = regexpTreeParser.parse.bind(regexpTreeParser);
+
+/**
+ * Parses a regular expression.
+ *
+ * Override original `regexpTreeParser.parse` to convert a value to a string,
+ * since in regexp-tree we may pass strings, and RegExp instance.
+ */
+regexpTreeParser.parse = function(regexp, options) {
+  return generatedParseFn(`${regexp}`, options);
+};
+
 // By default do not capture locations; callers may override.
 regexpTreeParser.setOptions({captureLocations: false});
 
